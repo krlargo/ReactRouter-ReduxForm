e042e2c9d189821e83d08b8992ce1bf367bb0525
krlargo <krlargo@ucdavis.edu>
2017-12-27 20:55:08 -0800

    Delete test code

--- src/index.js
+++ src/index.js
@@ -7,30 +7,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 // BrowserRouter interacts with History library
 // Route can be rendered inside any component
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render() {
-    return <div>Hello</div>
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Goodbye</div>
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        ROUTER SAMPLE
-        <Route path="/hello" component={Hello}/>
-        <Route path="/goodbye" component={Goodbye}/>
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
