405c905da4310de5c848dece752f415e9759792d
krlargo <krlargo@ucdavis.edu>
2018-01-01 14:08:19 -0800

    Use general renderField() instead

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-renderTitleField(field) {
-  return (
-    <div>
-      <input
-        type="text"
-        {...field.input}
-      />
-    </div>
-  )
-}
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.labelText}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
 
   render() {
     return (
       <div>
         <form>
           <Field
+            labelText="Title"
             name="title"
-            component={this.renderTitleField}
+            component={this.renderField}
+          />
+
+          <Field
+            labelText="Tags"
+            name="tags"
+            component={this.renderField}
+          />
+
+          <Field
+            labelText="Content"
+            name="content"
+            component={this.renderField}
           />
         </form>
       </div>
