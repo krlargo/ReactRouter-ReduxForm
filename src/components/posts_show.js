1f645428a743360e10ea9dc7ac30e57fe9335e21
krlargo <krlargo@ucdavis.edu>
2018-01-02 02:17:09 -0800

    Add PostsShow component

---
+++ src/components/posts_show.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class PostsShow extends Component {
+  render() {
+    return (
+/*    const { title, categories, content } = this.props.post;
+
+    return (
+      <div>
+        <Link
+          className="btn btn-primary"
+          to="/">
+          < Back to Posts
+        </Link>
+        <div>{title}</div>
+        <div>{categories}</div>
+        <div>{content}</div>
+      </div>
+    )*/
+
+      <div>PostsShow!</div>
+    )
+  }
+}
+
+//export default connect({ post })(PostsShow);
+export default PostsShow;
