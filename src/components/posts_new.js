3a46b1026859172aa644a08cd46fc56caadadf69
krlargo <krlargo@ucdavis.edu>
2018-01-02 00:53:22 -0800

    Handle post creation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -26,7 +28,9 @@ class PostsNew extends Component {
 
   // Gets called AFTER validation
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -89,4 +93,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
