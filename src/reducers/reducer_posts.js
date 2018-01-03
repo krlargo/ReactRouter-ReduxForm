a4cbe5001fa085e6cf782b80449e9962cc539dc8
krlargo <krlargo@ucdavis.edu>
2018-01-02 22:01:45 -0800

    Add handler for DELETE_POST action

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,17 +1,14 @@
-import { FETCH_POST, FETCH_POSTS } from '../actions';
+import { FETCH_POST, FETCH_POSTS, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_POST:
-      //const post = action.payload.data;
-      //const newState = { ...state };
-      //newState[post.id] = post;
-      //return newState;
-
       return { ...state, [action.payload.data.id]: action.payload.data }
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return _.omit(state, action.payload); // Payload in this case *is* 'id'
   default:
     return state;
   }
