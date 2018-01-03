c19725e3055b7789174f30d2858d29a5469bd351
krlargo <krlargo@ucdavis.edu>
2018-01-02 17:49:50 -0800

    Setup post retrieval using url id

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // Provided by ReactRouter
+    // 'params' gives us wild card values in our URL; extract 'id' specifically
+    const { id } = this.props.match.params;
+    this.props.fetchPost();
+  }
+
   render() {
-    return (
-/*    const { title, categories, content } = this.props.post;
+    const { title, categories, content } = this.props.post;
 
     return (
       <div>
         <Link
           className="btn btn-primary"
           to="/">
-          < Back to Posts
+          Back to Posts
         </Link>
         <div>{title}</div>
         <div>{categories}</div>
         <div>{content}</div>
       </div>
-    )*/
-
-      <div>PostsShow!</div>
     )
   }
 }
 
-//export default connect({ post })(PostsShow);
-export default PostsShow;
+// 'ownProps' === 'this.props';
+// this is how you access it in mapStateToProps()
+// Used for intermediate calculations
+function mapStateToProps({ posts }, ownProps) {
+  return {
+    post: posts[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
